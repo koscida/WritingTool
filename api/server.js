be5f60b376c0f85f3a5b9af626fa8e70f89a57f7
koscida <britkos@gmail.com>
2022-10-27 19:24:36 -0400

    adding routers to api

--- api/server.js
+++ api/server.js
@@ -1,22 +1,67 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const path = require('node:path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const cors = require("cors");
+const path = require("node:path");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static(path.resolve(__dirname, '../client/public')));
+// get routers
+const project = require(__dirname + "/routes/project.js");
+//
+const character = require(__dirname + "/routes/character.js");
+const organization = require(__dirname + "/routes/organization.js");
+const culture = require(__dirname + "/routes/culture.js");
+const artifact = require(__dirname + "/routes/artifact.js");
+//
+const magic = require(__dirname + "/routes/magic.js");
+const language = require(__dirname + "/routes/language.js");
+const location = require(__dirname + "/routes/location.js");
+const atlas = require(__dirname + "/routes/atlas.js");
+const timeline = require(__dirname + "/routes/timeline.js");
+//
+const profile = require(__dirname + "/routes/profile.js");
+//
 
+// app
+const app = express();
 
-app.get("/api", function(req, res) {
-  res.send({"name": "Jane Doe"}) // Should be json format
-})
+// parse request of body
+app.use(bodyParser.urlencoded({ extended: true }));
+// use public static folder
+app.use(express.static("public"));
+// set view engine
+app.set("view engine", "ejs");
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+// connect to mongodb
+const mongooseDB = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongooseDB);
+
+// test mongoose connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+	console.log("Connected successfully");
 });
 
+// include the routers for each sub page
+app.use("/project", project);
+//
+app.use("/character", character);
+app.use("/organization", organization);
+app.use("/culture", culture);
+app.use("/artifact", artifact);
+//
+app.use("/magic", magic);
+app.use("/language", language);
+app.use("/location", location);
+app.use("/atlas", atlas);
+app.use("/timeline", timeline);
+//
+app.use("/profile", profile);
 
+// listen
 app.listen(3000, () => {
-  console.log("App listening on port 3000")
-})
+	console.log("App listening on port 3000");
+});
---
+++ client/src/components/project/language/Letters.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Letters extends React.Component {
+	render() {
+		return (
+			<>
+				<div className="row">
+					<div>
+						<h1>Letters</h1>
+					</div>
+				</div>
+				<div className="row">
+					<p>Sentence Structure</p>
+					<p>Noun Phrase Order</p>
+					<p>Plurization</p>
+					<p>Conjugation</p>
+					<p>Negation</p>
+					<p>Inflection</p>
+				</div>
+			</>
+		);
+	}
+}
+
+export default Letters;
